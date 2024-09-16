7b5a22830763575c6f2f476a6b124dcfa984d591
Yui <140684321+YuiGasuki@users.noreply.github.com>
2024-09-17 00:07:53 +0800

    Add files via upload

--- Azuplayer.js
+++ Azuplayer.js
@@ -77,7 +77,7 @@ function player(Object){
     suspendedBallHtml = `
     <div id="azuplayer-black-main"></div>
     <div id="azuplayer-box-suspendedBall" style="position: fixed;${a}">
-    <img id="azuplayer-suspendedBall-img" src="${OriginalPath}azuplay-pause.svg" alt="播放按钮">
+    <div id="azuplayer-suspendedBall-img" alt="播放按钮">${azuplayPauseSvg}</div>
     <img class="azuplayer-cover-img" id="azuplayer-suspendedBall-cover" src="${urlCover}" alt="封面">
     </div>
     `;
@@ -97,7 +97,7 @@ function player(Object){
     }
     </style>
     <div id="azuplayer-box-main" ${suspendedBallCss}>
-        <img id="azuplayer-play-img" src="${OriginalPath}azuplay-pause.svg" alt="播放按钮" class="azuplayer-play-img">
+        <div id="azuplayer-play-img" alt="播放按钮" class="azuplayer-play-img">${azuplayPauseSvg}</div>
         <a href="${urlaudio}" id="azuplayer-play-a">
         <img id="azuplayer-cover-img" src="${urlCover}" alt="封面" class="azuplayer-cover-img">
         </a>
@@ -178,9 +178,9 @@ function player(Object){
     }
     PlayAudio.onloadedmetadata = () =>{    
         PlayAudio.pause();
-        PlayImg.src=OriginalPath+"azuplay-pause.svg";
+        PlayImg.innerHTML=azuplayPauseSvg;
         if(data.SuspendedBall){
-            azuplayerSuspendedBallImg.src=OriginalPath+"azuplay-pause.svg";
+            azuplayerSuspendedBallImg.innerHTML=azuplayPauseSvg
         }
         ProgressRanged.max = PlayAudio.duration;
         PlaytimeShow.innerText = `${formattingTime(PlayAudio.currentTime)}/${formattingTime(PlayAudio.duration)}`;
@@ -199,9 +199,9 @@ function player(Object){
     });
     PlayAudio.onended = () =>{
         PlayAudio.pause();
-        PlayImg.src=OriginalPath+"azuplay-pause.svg";
+        PlayImg.innerHTML=azuplayPauseSvg;
         if(data.SuspendedBall){
-            azuplayerSuspendedBallImg.src=OriginalPath+"azuplay-pause.svg";
+            azuplayerSuspendedBallImg.innerHTML=azuplayPauseSvg
         }
     }
     PlayButton.addEventListener('click',playerplay);
@@ -212,15 +212,15 @@ function player(Object){
         }
         if(PlayAudio.paused){
             PlayAudio.play();
-            PlayImg.src=OriginalPath+"azuplay-play.svg";
+            PlayImg.innerHTML = azuplayPlaySvg;
                     if(data.SuspendedBall){
-            azuplayerSuspendedBallImg.src=OriginalPath+"azuplay-play.svg";
+            azuplayerSuspendedBallImg.innerHTML=azuplayPlaySvg;
         }
         }else{
             PlayAudio.pause();
-            PlayImg.src=OriginalPath+"azuplay-pause.svg";
+            PlayImg.innerHTML = azuplayPauseSvg;
                     if(data.SuspendedBall){
-            azuplayerSuspendedBallImg.src=OriginalPath+"azuplay-pause.svg";
+            azuplayerSuspendedBallImg.innerHTML=azuplayPauseSvg
         }
         }
     }
@@ -270,9 +270,9 @@ function player(Object){
 player.prototype.play = function (){
     if(this.PlayAudio){
         this.PlayAudio.play();
-        this.PlayImg.src=OriginalPath+"azuplay-play.svg";
+        this.PlayImg.innerHTML = azuplayPlaySvg;
         if(data.SuspendedBall){
-            this.azuplayerSuspendedBallImg.src=OriginalPath+"azuplay-play.svg";
+            this.azuplayerSuspendedBallImg.innerHTML=azuplayPlaySvg;
         }
         return "ok"
     }else{
@@ -282,9 +282,9 @@ player.prototype.play = function (){
 player.prototype.pause = function (){
     if(this.PlayAudio){
         this.PlayAudio.pause();
-        this.PlayImg.src=OriginalPath+"azuplay-pause.svg";
+        this.PlayImg.innerHTML=azuplayPauseSvg;
         if(data.SuspendedBall){
-            this.azuplayerSuspendedBallImg.src=OriginalPath+"azuplay-pause.svg";
+            this.azuplayerSuspendedBallImg.innerHTML=azuplayPauseSvg
         }
         return "ok"
     }else{
@@ -319,3 +319,9 @@ const formattingTime = (Number) =>{
 }
 
 
+/**
+ *储存所需svg
+ */
+const azuplayPlaySvg = `<svg t="1722268993428" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1440" xmlns:xlink="http://www.w3.org/1999/xlink" width="22" height="22"><path d="M511.3 98.3c-228 0-413.5 185.5-413.5 413.5 0 23.8 2.6 48.1 6.5 70.8 0 0.1 0.2 0.1 0.3 0.2 1.6 5.8 6.7 10.1 13 10.1 7.6 0 13.8-6.2 13.8-13.8 0-0.5-0.2-0.9-0.3-1.3l0.2-0.1c-3.7-21.4-6-43.3-6-65.8 0-213.1 172.8-385.9 385.9-385.9s385.9 172.8 385.9 385.9-172.6 385.9-385.8 385.9c-142.4 0-266.5-77.4-333.4-192.2 0-0.1-0.1-0.1-0.1-0.2l-0.6-0.9c-2.3-4.3-6.7-7.4-12-7.4-7.6 0-13.8 6.2-13.8 13.8 0 3 1.2 5.6 2.8 7.8l-0.2 0.1C225.7 842 358.8 925.2 511.3 925.2c228 0 413.5-185.5 413.5-413.5S739.3 98.3 511.3 98.3z" fill="#aaaaaa" p-id="1441"></path><path d="M432.1 672.1c-7.6 0-13.8-6.2-13.8-13.8V365.4c0-7.6 6.2-13.8 13.8-13.8 7.6 0 13.8 6.2 13.8 13.8v292.9c0 7.6-6.2 13.8-13.8 13.8zM590.6 672.1c-7.6 0-13.8-6.2-13.8-13.8V365.4c0-7.6 6.2-13.8 13.8-13.8 7.6 0 13.8 6.2 13.8 13.8v292.9c0 7.6-6.2 13.8-13.8 13.8z" fill="#aaaaaa" p-id="1442"></path></svg>`;
+
+const azuplayPauseSvg = `<svg t="1722244541595" viewBox="0 0 1024 1024" version="1.1" p-id="4396"  width="22" height="22"><path d="M675.328 117.717333A425.429333 425.429333 0 0 0 512 85.333333C276.352 85.333333 85.333333 276.352 85.333333 512s191.018667 426.666667 426.666667 426.666667 426.666667-191.018667 426.666667-426.666667c0-56.746667-11.093333-112-32.384-163.328a21.333333 21.333333 0 0 0-39.402667 16.341333A382.762667 382.762667 0 0 1 896 512c0 212.074667-171.925333 384-384 384S128 724.074667 128 512 299.925333 128 512 128c51.114667 0 100.8 9.984 146.986667 29.12a21.333333 21.333333 0 0 0 16.341333-39.402667zM456.704 305.92C432.704 289.152 405.333333 303.082667 405.333333 331.797333v360.533334c0 28.586667 27.541333 42.538667 51.370667 25.856l252.352-176.768c21.76-15.253333 21.632-43.541333 0-58.709334l-252.373333-176.768z m-8.597333 366.72V351.466667l229.269333 160.597333-229.269333 160.597333z" fill="#aaaaaa" p-id="4397"></path></svg>`;
